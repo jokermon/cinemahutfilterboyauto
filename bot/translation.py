8589a59e819b4381a1de762377535ad801cea43d
jokermon <85845434+jokermon@users.noreply.github.com>
2021-09-29 21:45:52 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,7 +4,7 @@
 
 class Translation(object):
     
-     START_TEXT = """<b>Hey {}!!</b>
+    START_TEXT = """<b>Hey {}!!</b>
 
 <b>ഞാൻ <b><i><a href="https://t.me/cinehut">Cinehut</a></i></b> എന്ന ഗ്രൂപ്പിൽ മൂവീസ് കൊടുക്കുന്ന ബോട്ട് ആണ് 
  
@@ -13,43 +13,17 @@ class Translation(object):
  🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <b><i><a href="https://t.me/kuruthi_amzonprime">joker</a></i></b>
 """    
     
-     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@cinehut
+    HELP_TEXT = """
+<b>നീ ഏതാ..... ഒന്ന് പോടെയ് അവൻ help ചോയ്ച്ച് വന്നിരിക്കുന്നു😤...I'm Different Bot U Know</b>
 """
     
-     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>○ ᴍʏ ɴᴀᴍᴇ</b> :  THALAPATHY
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/cinehut">jokersir</a></i></b>
-<b>➥ Language</b> : <code>Python3</code>
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-<b>➥ Source Code</b> : <i><a href="https://t.me/onnpoderka/2">Click Me</a></i>
+<b>○ ʟᴀɴɢᴜᴀɢᴇ</b> : ᴘʏᴛʜᴏɴ
+
+<b>○ ғʀᴀᴍᴇᴡᴏʀᴋ</b> : ᴘʏʀᴏɢʀᴀᴍ
+
+<b>○ ᴄʀᴇᴀᴛᴏʀ</b> : <b><i><a href="https://t.me/Angelamego">joker</a></i></b>
+
+<b>○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ</b> : 🔐
 """
