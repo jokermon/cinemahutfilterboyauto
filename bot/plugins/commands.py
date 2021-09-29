62bf804e6635a5baa7325e2b196bf092d4a52171
jokermon <85845434+jokermon@users.noreply.github.com>
2021-09-29 21:54:36 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -117,7 +117,7 @@ async def start(bot, update):
     
     await bot.send_photo(
         chat_id=update.chat.id,
-        photo="https://telegra.ph/file/0a8a85ea0753bcafbde8b.jpg",
+        photo="https://telegra.ph/file/2b1f0627abc51470cfb12.jpg",
         caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
         reply_markup=reply_markup,
